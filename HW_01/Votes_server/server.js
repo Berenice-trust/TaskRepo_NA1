a1d03fadd0ec92b7c2531c538b2bf6b65a224aae
Berenice-trust <berenice.trust@gmail.com>
2025-03-31 14:18:06 +0300

    fix server

--- HW_01/Votes_server/server.js
+++ HW_01/Votes_server/server.js
@@ -73,5 +73,5 @@ app.get('/', (req, res) => {
 
 // Запускаем сервер
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://5.187.3.57:${PORT}`);
 });
