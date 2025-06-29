ff8a2b66b77a8d034acd2661fe98cf7acd4fe349
Veranika Zhytnik <berenice.trust@gmail.com>
2025-06-29 09:30:35 +0000

    backup script for linux and backups/ in gitIgnore

---
+++ Novelune/backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+TIMESTAMP=$(date +"%F-%H-%M")
+BACKUP_DIR="/home/root/TaskRepo_NA1/Novelune/backups"
+mkdir -p $BACKUP_DIR
+
+# Бэкап базы данных
+mysqldump -u root -p'234441' novelune > $BACKUP_DIR/novelune-$TIMESTAMP.sql
+
+# Бэкап загруженных файлов
+cp -r /home/root/TaskRepo_NA1/Novelune/client/uploads $BACKUP_DIR/uploads-$TIMESTAMP
